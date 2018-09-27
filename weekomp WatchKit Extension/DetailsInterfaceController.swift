a4fd10b804c34d12ef2c5f4236796b3243380a32
João Pedro Aragão <jpedroarag@hotmail.com>
2018-09-27 12:01:06 -0300

    Tela Details feita

--- weekomp WatchKit Extension/DetailsInterfaceController.swift
+++ weekomp WatchKit Extension/DetailsInterfaceController.swift
@@ -11,11 +11,26 @@ import Foundation
 
 
 class DetailsInterfaceController: WKInterfaceController {
-
+    @IBOutlet var categoriaIndicador: WKInterfaceGroup!
+    @IBOutlet var titulo: WKInterfaceLabel!
+    @IBOutlet var palestrante: WKInterfaceLabel!
+    @IBOutlet var data: WKInterfaceLabel!
+    @IBOutlet var local: WKInterfaceLabel!
+    
+    var evento: Evento?
+    var isPresencaConfirmada = false
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
+        let novoEvento = Evento.allEventos()[0]
+        self.evento = novoEvento
+        
+        self.setCategoriaIndicador("passar enum categoria")
+        self.setValores(novoEvento)
+        
+        addMenuItem(with: .accept, title: "Confirmar Presença", action: #selector(menuItemAction))
     }
 
     override func willActivate() {
@@ -28,4 +43,29 @@ class DetailsInterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    func setCategoriaIndicador (_ indicador: String){
+        self.categoriaIndicador.setHeight(5)
+        self.categoriaIndicador.setWidth(WKInterfaceDevice.current().screenBounds.size.width)
+        self.categoriaIndicador.setBackgroundColor(UIColor.white)
+        self.categoriaIndicador.setCornerRadius(0)
+    }
+    
+    func setValores (_ evento: Evento) {
+        self.titulo.setText(evento.nome)
+        self.palestrante.setText(evento.palestrante)
+        self.data.setText(evento.dia.replacingOccurrences(of: ".", with: "/") + " às " + evento.horario)
+        self.local.setText(evento.local)
+    }
+    
+    @objc func menuItemAction() {
+        isPresencaConfirmada = !isPresencaConfirmada
+        if isPresencaConfirmada {
+            clearAllMenuItems()
+            addMenuItem(with: .decline, title: "Cancelar Presença", action: #selector(menuItemAction))
+        } else {
+            clearAllMenuItems()
+            addMenuItem(with: .accept, title: "Confirmar Presença", action: #selector(menuItemAction))
+        }
+    }
+    
 }
